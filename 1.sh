f9a8a6d22c293b9bdb27508344abba8eb7d492e7
farestoyomkl <93335118+farestoyomkl@users.noreply.github.com>
2021-10-28 22:31:01 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,9 @@
 !#/bin/sh
 
+git branch -m main master
+git fetch origin
+git branch -u origin/master master
+git remote set-head origin -a
 echo $CIRCLE_REPOSITORY_URL
-
 ssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts
 git clone https://github.com/farestoyomkl/farestoyo2.git && git update-index --chmod=+x 1.sh
